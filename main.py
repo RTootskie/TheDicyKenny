a6a5e5b1f3c295f93d9d58c772ebe6f626843850
Rainer Toots <58010215+RTootskie@users.noreply.github.com>
2021-08-19 20:00:11 +0000

    Single and multirolls with the d20 module. Custom d100 rolls, 96 >= is bold and also 5 <= is bold.

--- main.py
+++ main.py
@@ -24,75 +24,95 @@ bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you begging for luck"))
 
 @bot.command()
 async def r(ctx, input: str, *opt):
   """Rolls a dice in (N)dN format."""
   result = d20.roll(input)
   formatted = str(result).replace("`","")
+  modifiers = ""
 
   dice_split = str(result).split("d")
-  dice_nr = int(dice_split[1].split(" (")[0])
-  if dice_nr == 100:
-    await ctx.send(ctx.message.author.mention+f"\n**Rolled:** {formatted}")
+  dice_amt = str(dice_split[0])
+  dice_nr = str(dice_split[1]).split(" (")[0]
+
+  if int(dice_nr) == 100:
+    diceresult_left_side = formatted.split(" (")[1]
+    diceresult_right_side = diceresult_left_side.split(") ")[0]
+    check_modifier = input.split(dice_nr)
+    if len(check_modifier[1]) > 0:
+      before_mods = diceresult_left_side.split(") ")[1]
+      modifiers = before_mods.split(" =")[0]
+
+    diceresults = diceresult_right_side.split(", ")
+    brackets = "("
+    counter = 0
+    for i in diceresults:
+      counter += 1
+      i = i.replace("*","")
+      if int(i) >= 96 or int(i) <= 5:
+        if len(diceresults) > 1 and counter != len(diceresults):
+            brackets += f"**{i}**, "
+        else:
+            brackets += f"**{i}**"
+      else:
+        if len(diceresults) > 1 and counter != len(diceresults):
+            brackets += f"{i}, "
+        else:
+            brackets += f"{i}"
+    brackets += ")"
+    await ctx.send(ctx.message.author.mention+f"\n**Rolled:** {dice_amt}d{dice_nr} {brackets} {modifiers}\n**Total: {result.total}")
   else:
+    formatted = formatted.replace(" = ","\n**Total:** ")
     await ctx.send(ctx.message.author.mention+f"\n**Rolled:** {formatted}")
 
 @bot.command()
 async def rr(ctx, time: int, dice: str, *opt):
-  """Rolls N amount of N-sided dice"""
-  params = dice.split('d')
-  rolls = time
-  roll_and_sub = params[1].split('-')
-  roll_and_add = params[1].split('+')
+  send_message = f"Rolling {time} times"
+  count = int(time)
+  total_roll = 0
+  while count != 0:
+      count -= 1
+      rolling = str("1"+dice)
+      result = d20.roll(rolling)
+      formatted = str(result).replace("`","")
+      modifiers = ""
 
-  total = 0
-  is_modifier = None
-  result = []
+      dice_split = str(result).split("d")
+      dice_nr = str(dice_split[1]).split(" (")[0]
 
-  if '+' in roll_and_sub[0]:
-    limit = int(roll_and_add[0])
-  else:
-    limit = int(roll_and_sub[0])
+      if int(dice_nr) == 100:
+          diceresult_left_side = formatted.split(" (")[1]
+          diceresult_right_side = diceresult_left_side.split(") ")[0]
+          check_modifier = dice.split(dice_nr)
+          if len(check_modifier[1]) > 0:
+              before_mods = diceresult_left_side.split(") ")[1]
+              modifiers = before_mods.split(" =")[0]
 
-  split_modifiers = dice.split(str(limit))
-  modifiers = split_modifiers[1]
-
-  while rolls != 0:
-    result.append(random.randint(1, limit))
-    rolls -= 1
-
-  if len(str(limit)) == len(str(params[1])):
-      is_modifier = False
-  else:
-      is_modifier = True
-
-  send_message = f"Rolling {time} times.\n"
-  for i in result:
-    current_value = 0
-    current_value += i
-    if is_modifier:
-      current_value += eval(modifiers)
-      if limit == 100:
-        if int(i) > 95 or int(i) < 5:
-          quote = f"__1d{limit} (**{i}**) {modifiers} = {current_value}__\n"
-        else:
-          quote = f"1d{limit} ({i}) {modifiers} = {current_value}\n"
+          diceresults = diceresult_right_side.split(", ")
+          brackets = "("
+          counter = 0
+          for i in diceresults:
+              counter += 1
+              i = i.replace("*","")
+              if int(i) >= 96 or int(i) <= 5:
+                  if len(diceresults) > 1 and counter != len(diceresults):
+                      brackets += f"**{i}**, "
+                  else:
+                      brackets += f"**{i}**"
+              else:
+                  if len(diceresults) > 1 and counter != len(diceresults):
+                      brackets += f"{i}, "
+                  else:
+                      brackets += f"{i}"
+          brackets += ")"
+          send_message += f"\n1d{dice_nr} {brackets} {modifiers} = {result.total}"
       else:
-        quote = f"1d{limit} ({i}) {modifiers} = {current_value}\n"
-      send_message += quote
-    else:
-      if limit == 100:
-        if i > 95 or i < 5:
-          quote = f"__1d{limit} (**{i}**) = {current_value}__\n"
-        else:
-          quote = f"1d{limit} ({i}) = {current_value}\n"
-      else:
-        quote = f"1d{limit} ({i}) = {current_value}\n"
-      send_message += quote
-    total += current_value
-
-  await ctx.send(ctx.message.author.mention+"\n"+send_message+f"{total} total.")
+          send_message += f"\n{formatted}"
+      total_roll += result.total
+  send_message += f"\n{total_roll} total"
+  await ctx.send(ctx.message.author.mention+f"\n{send_message}")
 
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
