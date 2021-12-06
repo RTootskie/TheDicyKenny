4412bb99c162283c8f048942b171bbe797a972b4
Rainer Toots <58010215+RTootskie@users.noreply.github.com>
2021-12-06 20:24:53 +0000

    Bringing up to date

---
+++ cogs/database_pandas.py
@@ -0,0 +1,83 @@
+import logging
+import pandas as pd
+import numpy as np
+
+from replit import db
+from resources.players import players, main_game
+from discord.ext import commands
+
+# Logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='./logs/database_functionalities.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+# Logging
+
+# DB Structure
+"""
+db["all_dicy_data"] = {
+  "Dicy_Data": [0,0,0,0,0],
+  "temp_account": [0,0,0,0,0],
+} # Total Rolls - High Rolls - >60 Rolls - Low Rolls <40 Rolls
+"""
+# DB Keys
+# print(db.keys())
+# print(db["all_dicy_data"])
+
+# Initialize the data from remote database
+def generate_local_database():
+  total_data = {}
+  for data in db["all_dicy_data"]:
+    total_data[data] = []
+    for sub_data in db["all_dicy_data"][data]:
+      total_data[data].append(sub_data)
+
+  df = pd.DataFrame(total_data,
+                    index=[
+                      "Total Rolls",
+                      "High Rolls",
+                      ">60 Rolls",
+                      "<40 Rolls",
+                      "Low Rolls"
+                      ])
+  return df
+
+class DiceData(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+      print("Cog database loaded")
+
+    @commands.command(aliases=["database","db","data"])
+    async def _data(self, ctx, target="None"):
+      """Gives you an overview of the dice rolls inside the database. Usage: *statistics [None,All,Specific Player] Will need to use Discord name. None- Global results, All- Global and Players, Specifc Player - Only the player's results"""
+
+      data = generate_local_database()
+      message = ""
+      if target == "None":
+        message += "*The global data:*\n"
+        message += data["Dicy_Data"].to_string()
+        print(data["Dicy_Data"].to_string())
+      elif target.lower() == "all":
+        message += "*The global and player data:*\n"
+        players_to_list = ['Dicy_Data']
+        for player in main_game:
+          players_to_list.append(player)
+
+        message += data[players_to_list].to_string()
+        print(data[players_to_list].to_string())
+      else:
+        if target in players:
+          message += f"*The data of **{target}** rolls*\n"
+          message += data[target].to_string()
+          print(data[target].to_string())
+        else:
+          message = "That player does not exist in our database."
+
+      await ctx.send(ctx.message.author.mention +f"\n{message}")
+
+def setup(bot):
+    bot.add_cog(DiceData(bot))
--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os
 import discord
 import logging
 
+from replit import db
 from discord.ext import commands
 
 # Custom imports
 from resources.dice_functions import dice_bot_logic
+from resources.players import players
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='./logs/discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename='./logs/discord.log',
+                              encoding='utf-8',
+                              mode='w')
+handler.setFormatter(
+logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='*', description='A Kenny that handles all your dice needs.', intents=intents)
+bot = commands.Bot(command_prefix='*',
+                   description='A Kenny that handles all your dice needs.',
+                   intents=intents)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
@@ -27,7 +34,8 @@ for filename in os.listdir('./cogs'):
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Brutal mass battle!"))
+    await bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name="Brutal mass battle!"))
 
 
 # TODO Create a message constructor so the command r simply goes through the list of results and prints the message accordingly
@@ -36,28 +44,78 @@ async def on_ready():
 # TODO Create a new functionality for the dice logic to roll an iteration and add bonuses to each rolls
 # TODO Make sure that if a modifier has another D then you can roll again.
 
+def add_entries_to_database(dice_roll, size_of_dice, author):
+  db["all_dicy_data"]["Dicy_Data"][0] += 1
+  if author in players:
+    db["all_dicy_data"][author][0] += 1
+
+  #or dice_roll == max(1, size_of_dice)
+  #or dice_roll == min(1, size_of_dice)
+
+  if determine_d100(size_of_dice):
+    # Add to High Roll and > 60 Roll
+    if dice_roll > 95:
+        db["all_dicy_data"]["Dicy_Data"][1,2] += 1
+        if author in players:
+          db["all_dicy_data"][author][1,2] += 1
+    # Add to > 60 Roll        
+    elif dice_roll >= 60:
+      db["all_dicy_data"]["Dicy_Data"][2] += 1
+      if author in players:
+        db["all_dicy_data"][author][2] += 1
+    # Add to <40 Roll
+    elif dice_roll <= 40:
+      db["all_dicy_data"]["Dicy_Data"][3] += 1
+      if author in players:
+        db["all_dicy_data"][author][3] += 1
+    # Add to Low Roll and <40 Roll
+    elif dice_roll < 6:
+      db["all_dicy_data"]["Dicy_Data"][3,4] += 1
+      if author in players:
+        db["all_dicy_data"][author][3,4] += 1
+
+  else:
+    if dice_roll == max(1, size_of_dice):
+      db["all_dicy_data"]["Dicy_Data"][1] += 1
+      if author in players:
+        db["all_dicy_data"][author][1] += 1
+    elif dice_roll == min(1, size_of_dice):
+      db["all_dicy_data"]["Dicy_Data"][4] += 1
+      if author in players:
+        db["all_dicy_data"][author][4] += 1
+
+
 def determine_d100(size_of_dice):
     """Takes the input dice and returns true or false"""
     return int(size_of_dice) == 100
 
+# DB Structure
+"""
+db["all_dicy_data"] = {
+  "Dicy_Data": [0,0,0,0,0],
+  "temp_account": [0,0,0,0,0],
+} # Total Rolls - High Rolls - >60 Rolls - <40 Rolls - Low Rolls 
+"""
 
-def dice_are_open_ender(size_of_dice, list_of_dice):
+def dice_are_open_ender(size_of_dice, list_of_dice, author):
     """Receive a list of dice results and decide how they should be formatted."""
     message_variable = ""
     dice_iterator = 0
     for roll_result in list_of_dice:
-        dice_iterator += 1
-        if determine_d100(size_of_dice):  # Are D100s being rolled
-            if int(roll_result) > 95 or int(roll_result) < 6:
-                roll_result = f"**{roll_result}**"
-        else:
-            if int(roll_result) == min(1, size_of_dice) or int(roll_result) == max(1, size_of_dice):
-                roll_result = f"**{roll_result}**"
+      add_entries_to_database(roll_result, size_of_dice, author)
+      
+      dice_iterator += 1
+      if determine_d100(size_of_dice):  # Are D100s being rolled
+          if int(roll_result) > 95 or int(roll_result) < 6:
+              roll_result = f"**{roll_result}**"
+      else:
+          if int(roll_result) == min(1, size_of_dice) or int(roll_result) == max(1, size_of_dice):
+              roll_result = f"**{roll_result}**"
 
-        if int(dice_iterator) == len(list_of_dice):
-            message_variable += f"{roll_result}"
-        else:
-            message_variable += f"{roll_result}, "
+      if int(dice_iterator) == len(list_of_dice):
+          message_variable += f"{roll_result}"
+      else:
+          message_variable += f"{roll_result}, "
 
     return message_variable
 
@@ -66,21 +124,25 @@ def dice_are_open_ender(size_of_dice, list_of_dice):
 async def r(ctx, user_dice_string: str):
     """Rolls in the (N)dN format."""
     results_from_logic = dice_bot_logic(user_dice_string)
-    dice_results = dice_are_open_ender(results_from_logic["dice_size"], results_from_logic["dice_results"])
+    dice_results = dice_are_open_ender(results_from_logic["dice_size"],
+                                       results_from_logic["dice_results"],
+                                       ctx.message.author.name)
 
     if results_from_logic["modifiers"]:
-        await ctx.send(ctx.message.author.mention +
-                       f"\n**Rolled:** {results_from_logic['dice_num']}d{results_from_logic['dice_size']} " +
-                       f"({dice_results}) {results_from_logic['modifiers']}\n" +
-                       f"**Total:** {results_from_logic['roll_total']}")
+        await ctx.send(
+            ctx.message.author.mention +
+            f"\n**Rolled:** {results_from_logic['dice_num']}d{results_from_logic['dice_size']} "
+            + f"({dice_results}) {results_from_logic['modifiers']}\n" +
+            f"**Total:** {results_from_logic['roll_total']}")
     else:
-        await ctx.send(ctx.message.author.mention +
-                       f"\n**Rolled:** {results_from_logic['dice_num']}d{results_from_logic['dice_size']} " +
-                       f"({dice_results})\n" +
-                       f"**Total:** {results_from_logic['roll_total']}")
+        await ctx.send(
+            ctx.message.author.mention +
+            f"\n**Rolled:** {results_from_logic['dice_num']}d{results_from_logic['dice_size']} "
+            + f"({dice_results})\n" +
+            f"**Total:** {results_from_logic['roll_total']}")
 
 
-@bot.command()
+@bot.command(aliases=["RR"])
 async def rr(ctx, time: int, dice: str):
     """Rolls N amount of dice in (N) dN format."""
     send_message = f"Rolling {time} times"
@@ -89,7 +151,9 @@ async def rr(ctx, time: int, dice: str):
     while count != 0:
         count -= 1
         results_from_logic = dice_bot_logic(dice)
-        dice_results = dice_are_open_ender(results_from_logic["dice_size"], results_from_logic["dice_results"])
+        dice_results = dice_are_open_ender(results_from_logic["dice_size"],
+                                           results_from_logic["dice_results"],
+                                           ctx.message.author.name)
         if results_from_logic['modifiers']:
             send_message += f"\n1d{results_from_logic['dice_size']} ({dice_results}) {results_from_logic['modifiers']} = {results_from_logic['roll_total']}"
         else:
@@ -99,5 +163,10 @@ async def rr(ctx, time: int, dice: str):
     send_message += f"\n{total_sum} total"
     await ctx.send(ctx.message.author.mention + f"\n{send_message}")
 
+
+# @bot.command(aliases=["database"])
+# async def data(ctx):
+#   data = total_data
+#   await ctx.send(ctx.message.author.mention + f"\n{data}")
 
 bot.run(os.getenv('TOKEN'))
